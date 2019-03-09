0f8af683774d83edb1a45ff84a26c00f625f3038
umetaman <info@umetaman.com>
2019-03-09 20:29:18 +0900

    固定ページテンプレートの実装

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php
+    get_header();
+?>
+
+<div class="container">
+    <div class="contents">
+    <?php if(have_posts()): the_post(); ?>
+    <article <?php post_class('post-article'); ?>>
+        <!-- タイトル -->
+        <h1><?php the_title(); ?></h1>
+        <!-- 本文取得 -->
+        <?php the_content(); ?>
+    </article>
+    <?php endif; ?>
+    </div>
+</div>
+<?php
+    get_footer();
+?>
