71453c7db63c81f240188c380f24d8c8b3186b14
umetaman <info@umetaman.com>
2019-04-04 00:58:44 +0900

    開催場所の余計な情報を削除

--- tribe/events/blocks/parts/venue.php
+++ tribe/events/blocks/parts/venue.php
@@ -27,26 +27,4 @@ $website = tribe_get_venue_website_link();
 	<div class="tribe-block__venue__name">
 		<h3><?php echo tribe_get_venue() ?></h3>
 	</div>
-
-	<?php do_action( 'tribe_events_single_meta_venue_section_start' ) ?>
-
-	<?php if ( tribe_address_exists() ) : ?>
-		<address class="tribe-block__venue__address">
-			<?php echo tribe_get_full_address(); ?>
-
-			<?php if ( tribe_show_google_map_link() ) : ?>
-				<?php echo tribe_get_map_link_html(); ?>
-			<?php endif; ?>
-		</address>
-	<?php endif; ?>
-
-	<?php if ( ! empty( $phone ) ) : ?>
-		<span class="tribe-block__venue__phone"><?php echo $phone ?></span><br />
-	<?php endif ?>
-
-	<?php if ( ! empty( $website ) ) : ?>
-		<span class="tribe-block__venue__website"><?php echo $website ?></span><br />
-	<?php endif ?>
-
-	<?php do_action( 'tribe_events_single_meta_venue_section_end' ) ?>
 </div>
