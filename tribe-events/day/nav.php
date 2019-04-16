f642de6e84ef8935080c6949c89c3fd37c99b38b
umetaman <info@umetaman.com>
2019-04-16 18:03:21 +0900

    ナビゲーションのクラスをmyuに変更

--- tribe-events/day/nav.php
+++ tribe-events/day/nav.php
@@ -14,18 +14,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 } ?>
 
-<nav class="tribe-events-nav-pagination" aria-label="<?php esc_html_e( 'Day Navigation', 'the-events-calendar' ) ?>">
-	<ul class="tribe-events-sub-nav">
+<nav class="myu-events-nav-pagination" aria-label="<?php esc_html_e( 'Day Navigation', 'the-events-calendar' ) ?>">
+	<ul class="myu-events-sub-nav">
 
 		<!-- 前の日 -->
-		<li class="tribe-events-nav-previous">
+		<li class="myu-events-nav-previous">
 			<?php 
 			tribe_the_day_link( 'previous day', '<i class="fas fa-arrow-left"></i>'."前の日" );
 			 ?>
 		</li>
 
 		<!-- 次の日へ -->
-		<li class="tribe-events-nav-next">
+		<li class="myu-events-nav-next">
 			<?php 
 			tribe_the_day_link( 'next day', "次の日".'<i class="fas fa-arrow-right"></i>' );
 			?>
