db4d31631f721ce7df8c17ef807607c18f0bc2e0
umetaman <info@umetaman.com>
2019-04-16 23:27:46 +0900

    list表示のナビゲーションを整備

--- tribe-events/list/nav.php
+++ tribe-events/list/nav.php
@@ -19,22 +19,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 } ?>
 
-<nav class="tribe-events-nav-pagination" aria-label="<?php echo esc_html( sprintf( esc_html__( '%s List Navigation', 'the-events-calendar' ), $events_label_plural ) ); ?>">
-	<ul class="tribe-events-sub-nav">
-		<!-- Left Navigation -->
+<nav class="myu-events-nav-pagination" aria-label="<?php echo esc_html( sprintf( esc_html__( '%s List Navigation', 'the-events-calendar' ), $events_label_plural ) ); ?>">
+	<ul class="myu-events-sub-nav">
 
-		<?php if ( tribe_has_previous_event() ) : ?>
-			<li class="<?php echo esc_attr( tribe_left_navigation_classes() ); ?>">
-				<a href="<?php echo esc_url( tribe_get_listview_prev_link() ); ?>" rel="prev"><span>&laquo;</span> <?php echo esc_html( sprintf( __( 'Previous %s', 'the-events-calendar' ), $events_label_plural ) ); ?></a>
-
-			</li><!-- .tribe-events-nav-left -->
+		<?php 
+		// 前のイベントがあったとき
+		if ( tribe_has_previous_event() ) : ?>
+			<!-- 前のイベントへ -->
+			<li class="myu-events-nav-previous">
+				<a href="<?php echo esc_url(tribe_get_listview_prev_link()); ?>">
+					<i class="fas fa-arrow-left"></i>前のイベント
+				</a>
+			</li>
 		<?php endif; ?>
 
-		<!-- Right Navigation -->
-		<?php if ( tribe_has_next_event() ) : ?>
-			<li class="<?php echo esc_attr( tribe_right_navigation_classes() ); ?>">
-				<a href="<?php echo esc_url( tribe_get_listview_next_link() ); ?>" rel="next"><?php echo esc_html( sprintf( __( 'Next %s', 'the-events-calendar' ), $events_label_plural ) ); ?> <span>&raquo;</span></a>
-			</li><!-- .tribe-events-nav-right -->
+		<?php 
+		// 次のイベントがあったとき
+		if ( tribe_has_next_event() ) : ?>
+			<!-- 前のイベントへ -->
+			<li class="myu-events-next-previous">
+				<a href="<?php echo esc_url(tribe_get_listview_next_link()); ?>">
+					次のイベント<i class="fas fa-arrow-right"></i>
+				</a>
+			</li>
 		<?php endif; ?>
+
 	</ul>
 </nav>
