29ac7a64e8b766371e73723c65c3f3e7d942e9cf
umetaman <info@umetaman.com>
2019-05-23 10:36:23 +0900

    カテゴリー別一覧ページを追加

---
+++ category.php
@@ -0,0 +1,56 @@
+<?php
+// Template Name: Category
+?>
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="contents">
+        <h1 class="entry-title"><?php single_cat_title("カテゴリー別記事一覧: "); ?></h1>
+        <!-- 投稿された記事の一覧 -->
+        <ul id="myu-post-archives">
+        <?php
+        //すべての記事を取得
+        $post_params = array("category_name" => single_cat_title(null, false));
+        $posts = get_posts($post_params);
+
+        //繰り返しで出力
+        foreach($posts as $post):
+        setup_postdata($post);
+        ?>
+    
+        <li>
+            <div class="myu-post-archive-wrapper">
+                <?php
+                //サムネ画像が設定されていないとき、NoImageの画像に置き換える。
+                $thumbnail_url = get_template_directory_uri().'/images/no_image.jpg';
+                if(has_post_thumbnail()){
+                    $thumbnail_url = get_the_post_thumbnail_url();
+                }
+                ?>
+                <!-- 記事のサムネイル -->
+                <div class="myu-post-archive-thumbnail-wrapper">
+                    <img class="myu-post-archive-thumbnail" src="<?php echo $thumbnail_url; ?>" alt="<?php echo the_title(); ?>">
+                </div>
+                <!-- 記事のタイトル -->
+                <div class="myu-post-archive-info-wrapper">
+                    <h2 class="myu-post-archive-title"><?php the_title(); ?></h2>
+                    <!-- 日付 -->
+                    <p class="myu-post-archive-date"><?php the_date(); ?></p>
+                    <!-- カテゴリ -->
+                    <p class="myu-post-archive-category"><i class="fas fa-folder-open"></i><?php the_category(', '); ?></p>
+                    <!-- サムネ画像。持っていたら表示、持っていなかったらNoImage -->
+                    <p class="myu-post-archive-excerpt"><?php the_excerpt(); ?></p>
+                </div>
+                <a class="myu-post-archive-link" href="<?php the_permalink(); ?>"></a>
+            </div>
+        </li>
+
+        <?php
+        endforeach;
+        wp_reset_postdata();
+        ?>
+        </ul>
+    </div>
+</div>
+
+<?php get_footer(); ?>
