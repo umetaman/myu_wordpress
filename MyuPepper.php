bdce12a9fcc2490f00fb690dea0db4a5bde63c73
umetaman <info@umetaman.com>
2019-07-03 22:29:40 +0900

    プラグインの雛形を作成

---
+++ MyuPepper.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: MYU Pepper
+Plugin URI: http://dmc.myu.ac.jp/
+Description: 宮城大学データ＆メディアコモンズ専用のプラグイン。
+Author: umetaman
+version: Beta
+Author URI: http://umetaman.com/
+*/
+?>
+<?php
+
+class ShowText{
+    
+    function __construct(){
+
+    }
+
+}
+
+$show_text = new ShowText();
+
+?>
---
+++ plugin-test.php
@@ -0,0 +1,12 @@
+<?php
+// Template Name: PluginTest
+?>
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="contents">
+        <?php echo esc_html($show_text->get_text()); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
